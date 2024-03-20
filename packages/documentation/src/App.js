890502edf1e45f12576091c440e0dabc59a29e94
Daniel Morris <daniellmorris@gmail.com>
2024-03-19 21:59:36 -0500

    Update files

--- packages/documentation/src/App.js
+++ packages/documentation/src/App.js
@@ -1,12 +1,54 @@
-import { API } from '@stoplight/elements';
-
-import '@stoplight/elements/styles.min.css';
+import SwaggerUI from "swagger-ui-react"
+import AsyncApiComponent, { ConfigInterface } from "@asyncapi/react-component";
+import "swagger-ui-react/swagger-ui.css"
+import logo from './logo.svg';
 import './App.css';
+import conf from "./conf";
+import { useState } from "react";
+
+const config: ConfigInterface = {
+  "show": {
+    "sidebar": true,
+    "info": true,
+    "operations": true,
+    "servers": true,
+    "messages": true,
+    "schemas": true,
+    "errors": true
+  },
+  "expand":{
+    "messageExamples": false
+  },
+  "sidebar": {
+    "showServers": "byDefault",
+    "showOperations": "byDefault"
+  }
+}
 
 function App() {
+  // useState to store the selected value (Default to first item)
+  const [selectedValue, setSelectedValue] = useState(conf?.specOptions[0].value);
+  const onChange = (e) => {
+    // Set the selected value to the value of the selected option
+    // 
+    setSelectedValue(e.target.value);      
+  }
+  // Get config item based on selected value
+  const option = conf.specOptions.find(item => item.value === selectedValue);
   return (
-    <div className="App">
-      <API apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml" />
+    <div>
+      <div id="custom-topbar" style={{marginBottom: '20px', textAlign: 'right'}}>
+        <select id="custom-select-dropdown" value={selectedValue} onChange={onChange}>
+          { 
+            conf?.specOptions && conf.specOptions.map((item, index) => {
+              return <option key={index} value={item.value}>{item.label}</option>
+            })
+          }
+        </select>
+      </div>
+      { option.type === 'openapi' 
+        ? <SwaggerUI url={selectedValue} /> 
+        : <AsyncApiComponent url={selectedValue} config={config} /> }
     </div>
   );
 }
---
+++ packages/documentation/src/conf/index.js
@@ -0,0 +1,6 @@
+export default {
+  specOptions: [
+    { label: "API", value: "./openapi.yaml", type: 'openapi' },
+    { label: "Worker", value: "./asyncapi.yaml", type: 'asyncapi' },
+  ]
+}
